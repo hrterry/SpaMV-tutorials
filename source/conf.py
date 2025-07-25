9249576679dbf9b75b398d187fc426e4c583e09f
hrterry <2306125229@qq.com>
2025-07-25 11:34:42 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -30,7 +30,7 @@ html_static_path = ['_static']
 html_context = {
     'display_github': True,             # 启用 GitHub 链接
     'github_user': 'Enderlogic',
-    'github_repo': 'SpaMV',
-    'github_version': 'main',
+    'github_repo': 'SpaMV-tutorials',
+    'github_version': 'master',
     'conf_py_path': '/source/',
 }
