02f049188facb7258a387b3357caf7e092e6b9ea
hrterry <2306125229@qq.com>
2025-07-25 10:55:53 +0800

    edit on github button

--- source/conf.py
+++ source/conf.py
@@ -27,3 +27,10 @@ exclude_patterns = []
 # html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_context = {
+    'display_github': True,             # 启用 GitHub 链接
+    'github_user': 'hrterry',
+    'github_repo': 'SpaMV-tutorials',
+    'github_version': 'master',           # 分支名，如 main 或 master
+    'conf_py_path': '/source/',           # 文档目录在仓库中的路径，以 `/` 结尾
+}
